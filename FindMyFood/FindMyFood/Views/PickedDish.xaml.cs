735f866dde9de7e47916ea36dbc8a0ee2a6b15c5
AndreasIT <andr522f@edu.ucl.dk>
2020-05-03 13:59:16 +0200

    Tilføjede XAML for valgt ret. Den indeholder blandt andet en image resource.

---
+++ FindMyFood/FindMyFood/Views/PickedDish.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FindMyFood.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickedDish : ContentPage
+    {
+        public PickedDish()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //Der skal sendes et object videre fra sidste page, med alt der skal bruges
+            DishGrid.BindingContext = "Binding Virkede";
+        }
+    }
+}
