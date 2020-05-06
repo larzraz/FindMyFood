e58b8e21c938e731748c960581b16202fb07c2a2
larzraz <lars5030@edu.ucl.dk>
2020-05-06 19:08:49 +0200

    Added ValueConverter for the expirationday and updated the view for OwnFoodList to implent a bar with the color of the expirationdate for the item

---
+++ FindMyFood/FindMyFood/Views/ExpirationdateValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FindMyFood.Views
+{
+    public class ExpirationdateValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch(((DateTime)value-DateTime.Now).Days)
+                {
+                     case 0:
+                        return Color.Red;
+                    case 1:
+                    return Color.Yellow;
+                case 2:
+                    return Color.Yellow;
+                //case default:
+                    
+
+            }
+            return Color.Green;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
